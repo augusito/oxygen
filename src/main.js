f837966258c2cbe74b3843313e9e764a5a98a8b1
augusito <kaugustus15@gmail.com>
2022-11-21 14:42:58 +0300

    chore: add express

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
 (async () => {
-  console.log("Hello world!");
+  app.get("/", (req, res) => {
+    res.send("Hello world!");
+  });
+
+  app.listen(port, () => {
+    console.log(`Application is running on: ${port}`);
+  });
 })();
