3090ae87366b4a887b373d51545e16a3ccbaf867
augusito <kaugustus15@gmail.com>
2022-11-24 16:37:25 +0300

    feat: add validations to pageable

--- src/common/pageable.js
+++ src/common/pageable.js
@@ -1,10 +1,15 @@
+const { isFunction, isNull } = require("@hemjs/util");
 const { omit } = require("@hemjs/util/object");
 
 class Pageable {
   model;
   options;
 
-  constructor(model, options) {
+  constructor(model, options = {}) {
+    if (isNull(model)) {
+      throw new Error("Model must not be null");
+    }
+
     this.model = model;
     this.options = options;
   }
@@ -18,6 +23,20 @@ class Pageable {
   }
 
   async getItems() {
+    if (!isFunction(this.model?.count)) {
+      const object = this.toString(this.model);
+      throw new Error(
+        `${object} expects the provided model to implement count()`
+      );
+    }
+
+    if (!isFunction(this.model?.findAll)) {
+      const object = this.toString(this.model);
+      throw new Error(
+        `${object} expects the provided model to implement findAll()`
+      );
+    }
+
     const countOptions = omit(this.options, [
       "limit",
       "offset",
@@ -35,6 +54,33 @@ class Pageable {
       count,
       rows: count === 0 ? [] : rows,
     };
+  }
+
+  toString(value) {
+    if (typeof value === "string") {
+      return value;
+    }
+
+    if (Array.isArray(value)) {
+      return "[" + value.map(toString).join(", ") + "]";
+    }
+
+    if (value == null) {
+      return "" + value;
+    }
+
+    if (value.name) {
+      return `${value.name}`;
+    }
+
+    const result = value.toString();
+
+    if (result == null) {
+      return "" + result;
+    }
+
+    const newLineIndex = result.indexOf("\n");
+    return newLineIndex === -1 ? result : result.substring(0, newLineIndex);
   }
 }
 
